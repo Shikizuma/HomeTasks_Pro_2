44b5056ad1f419f504276185198b1ea1934bf36d
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-15 13:58:42 +0300

    Add Task6

---
+++ Task6/DescendingComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+	internal class DescendingComparer : IComparer<object>
+	{
+
+		public int Compare(object? x, object? y)
+		{
+			return Comparer.Default.Compare(y, x);
+		}
+	}
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			SortedList<string, string> sortedList = new SortedList<string, string>();
+
+			sortedList.Add("apple", "яблуко");
+			sortedList.Add("banana", "банан");
+			sortedList.Add("orange", "апельсин");
+
+			foreach (KeyValuePair<string, string> kvp in sortedList)
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+			}
+            Console.WriteLine(new string('-', 30));
+            DescendingComparer descendingComparer = new DescendingComparer();
+			SortedList<string, string> reversedSortedList = new SortedList<string, string>(sortedList, descendingComparer);
+
+			foreach (KeyValuePair<string, string> kvp in reversedSortedList)
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+			}
+		}
+	}
+}
